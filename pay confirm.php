ae1f1e11d04baf1a7bf08643da4aac76211eab64
Sowmya7842 <112716943+Sowmya7842@users.noreply.github.com>
2023-11-16 13:45:03 +0530

    Add files via upload

---
+++ pay confirm.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Payment Confirmed</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <h1>Payment Confirmed</h1>
+    </header>
+
+    <main>
+        <section class="confirmation-message">
+            <h2>Thank You for Your Payment!</h2>
+            <p>Your payment has been successfully processed.</p>
+            <p>An email confirmation has been sent to your inbox.</p>
+            <p>Order details:</p>
+            <!-- Display order details here if available -->
+            <p><a href="dashboard.html">Return to Dashboard</a></p>
+        </section>
+    </main>
+</body>
+
+</html>
