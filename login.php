3c94f41429dbdf779cd28385c696ae71c10399da
Sowmya7842 <112716943+Sowmya7842@users.noreply.github.com>
2023-11-16 13:42:34 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cake ordering system</title>
+    <style>
+        /* Basic styling for demonstration purposes */
+        /* ... (existing styles remain the same) ... */
+        nav {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 10px 0;
+        }
+        nav ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+        }
+        nav ul li {
+            display: inline-block;
+            margin-right: 20px;
+        }
+        nav ul li a {
+            color: #fff;
+            text-decoration: none;
+            padding: 10px 15px;
+            transition: background-color 0.3s ease;
+        }
+        nav ul li a:hover {
+            background-color: #555;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Welcome to online cake ordering system</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="ip1.php">Home</a></li>
+            <li><a href="select cake.php">select cake</a></li>
+            <li><a href="order confirmed.php">order confirmed</a></li>
+            <li><a href="payment method.php">payment</a></li>
+            <li><a href="pay confirm.php">pay confirm</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+    <div class="container">
+        <div class="main" id="home">
+            <h2>Welcome to cake ordering system!</h2>
+            <p>This is the home section.</p>
+        </div>
+        <div class="main" id="about">
+            <h2>About Me</h2>
+            <p>Info
