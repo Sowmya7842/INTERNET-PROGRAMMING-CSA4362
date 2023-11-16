09bf20aad084df01086825acb073a69045b84190
Sowmya7842 <112716943+Sowmya7842@users.noreply.github.com>
2023-11-16 13:44:17 +0530

    Add files via upload

---
+++ order confirmed.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Order Confirmed</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <h1>Order Confirmed</h1>
+    </header>
+
+    <main>
+        <section class="confirmation-message">
+            <h2>Thank You for Your Order!</h2>
+            <p>Your order has been successfully placed.</p>
+            <p>An email confirmation has been sent to your inbox.</p>
+            <p>Order details:</p>
+            <!-- Display order details here if available -->
+            <p><a href="dashboard.html">Return to Dashboard</a></p>
+        </section>
+    </main>
+</body>
+
+</html>
